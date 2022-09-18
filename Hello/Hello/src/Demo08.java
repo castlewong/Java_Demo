42136c032a3d7d71809b092c0fe510d301feed78
castlewong <swy0114@qq.com>
2022-09-18 23:22:58 +0800

    var

---
+++ Hello/Hello/src/Demo08.java
@@ -0,0 +1,12 @@
+public class Demo08 {
+    // int a,b,c;
+    // int a = 1, b= 2,c=3; // 程序可读性很差 写成三行较好
+    String name = "castle";
+
+    static double salary = 2500;
+    public static void main(String[] args) {
+        Demo08 demo08 = new Demo08();
+        System.out.println(demo08.name);
+        System.out.println(salary);
+    }
+}
